030cc75110aaa0be4f9bf2dd8f8cea55f6428378
superasperatus <39364220+superasperatus@users.noreply.github.com>
2018-06-28 19:02:40 +0200

    Removed: comments on file handling, removed db mentioning

--- geocoder.py
+++ geocoder.py
@@ -9,30 +9,9 @@ import os
 
 app=Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:standardna18ka@localhost/geo-coder'   #configuring the database connection; with the one I create in pg4
-db=SQLAlchemy(app) #creats an SQL alchemy object
-
-#for future: you need to create the db by importing 'from geocoder import db' and then running db.create_all() in cmd
-
-class Data(db.Model): #blueprint of a database already created in SQLAlchemy and then accessing the model
-    __tablename__="submitted_data"
-    id=db.Column(db.Integer, primary_key=True) #setting the id primary primary_key
-    address_=db.Column(db.String(250), unique=True) #creating an email column with 120 limit and requiring only unique values
-    name_=db.Column(db.String(100))
-    employees_=db.Column(db.Integer)
-    lattitude_=db.Column(db.Integer)
-    longitude_=db.Column(db.Integer)
-
 @app.route("/")
 def index():
     return render_template("index.html")
-
-
-#TO DO:
-#files-uploading-and downloading works like a charm!!!
-#connect the project to github and work on it from there as well.
-#Go through tutorials and find where he manipulates files and creates longitude and lattitude from CSV
-#make it look nice through some peachy css
 
 
 @app.route("/processed-uploaded-data", methods=['POST']) #to REDO REDO
@@ -40,8 +19,7 @@ def processed_data():
     global file
     if request.method=='POST':
        file=request.files["file"]
-       #file.save(secure_filename("uploaded"+file.filename))
-    nom=Nominatim(scheme="http")
+       nom=Nominatim(scheme="http")
     df=pandas.read_csv(file)
     df["Coordinates"]=df["Address"].apply(nom.geocode)
     df["Latitude"]=df["Coordinates"].apply(lambda x: x.latitude if x != None else None)
